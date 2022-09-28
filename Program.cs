02482990b81e4e4db8f312018bd11a84219b47d7
SvetlanaB <bsv211112@gmail.com>
2022-09-28 09:06:47 +0300

    Ex21 added

--- Program.cs
+++ Program.cs
@@ -51,7 +51,33 @@ void Ex19()
 void Ex21()
 {
 
+int x1 = N("x", "A");
+int y1 = N("y", "A");
+int z1 = N("z", "A");
+int x2 = N("x", "B");
+int y2 = N("y", "B");
+int z2 = N("z", "B");
+
+int N(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+}
+Ex21();
 
 void Ex23()
 {
@@ -64,4 +90,4 @@ void Ex23()
 
     Console.WriteLine(x+"- " + Math.Pow(x, 3));
 }
-Ex23();
+//Ex23();
